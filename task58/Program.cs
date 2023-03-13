0d632b963f73171b554491ed546a43fb022d3e40
vip-dacha86 <vip-dacha86@yandex.ru>
2023-03-13 09:34:21 +0500

    task58 произведение матриц

---
+++ task58/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4                                         18 20
+// 3 2 | 3 3           Результирующая матрица будет: 15 18
+
+using System;
+using static System.Console;
+Clear();
+Write("Введите количество строк 1 массива: ");
+int rowsA = int.Parse(ReadLine());
+Write("Введите количество столбцов 1 массива: ");
+int columnsA = int.Parse(ReadLine());
+Write("Введите количество строк 2 массива: ");
+int rowsB = int.Parse(ReadLine());
+Write("Введите количество столбцов 2 массива: ");
+int columnsB = int.Parse(ReadLine());
+if (columnsA != rowsB)
+{
+    WriteLine("Такие матирицы умножать нельзя!");
+    return;
+}
+
+int[,] A = GetArray(rowsA, columnsA, 0, 10);
+int[,] B = GetArray(rowsB, columnsB, 0, 10);
+PrintArray(A);
+WriteLine();
+PrintArray(B);
+WriteLine();
+PrintArray(GetMultiplicationMatrix(A, B));
+
+
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]}  ");
+        }
+        WriteLine();
+    }
+}
+
+int[,] GetMultiplicationMatrix(int[,] arrayA, int[,] arrayB)
+{
+    int[,] arrayC = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+    for (int i = 0; i < arrayA.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayB.GetLength(1); j++)
+        {
+            for (int k = 0; k < arrayA.GetLength(1); k++)
+            {
+                arrayC[i, j] += arrayA[i, k] * arrayB[k, j];
+            }
+        }
+    }
+    return arrayC;
+}
